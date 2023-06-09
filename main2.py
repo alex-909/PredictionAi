9d9e7913ecea3f9c3badafa68e7800b411d17609
alex-909 <alex.strauss05@gmx.net>
2023-06-09 16:02:49 +0200

    new models

--- main2.py
+++ main2.py
@@ -18,11 +18,12 @@ x_train, x_val, y_train, y_val = train_test_split(x,y, test_size=0.3)
 
 model = Sequential()
 model.add(Input(shape=(32, )))
+model.add(Dense(16, activation='linear'))
+model.add(Dense(12, activation='linear'))
 model.add(Dense(10, activation='linear'))
-model.add(Dense(32, activation='linear'))
 model.add(Dense(2, activation='relu'))
 
-test = [[5,2,-1.7999999999999998,5,3,7.1000000000000005,10,0,-1.6999999999999997,1,1,0.09999999999999964,13,-2,-7.2,-1,18,-7,-4.000000000000001,-15,-2,-0.7000000000000002,-6,-2,-2.1,-6,0,4.4,-1,-1,-4.7,-15]]
+test = [[10,1,-2.2,3,1,-5.100000000000001,-10,1,1.4999999999999991,-8,-2,-1.5999999999999996,3,-1,-3.3000000000000007,-4,13,1,1.9000000000000004,-1,-2,-4.300000000000001,-15,1,-1.6,-7,-2,-2.1000000000000005,-7,-2,-3.5,-3]]
 test = preprocessing.MinMaxScaler().fit_transform(x)
 
 
@@ -30,3 +31,13 @@ model.compile(optimizer='adam', loss='mse', metrics = ['accuracy'])
 
 model.fit(x_train, y_train, batch_size=64, epochs=15, validation_data=(x_val, y_val))
 keras.utils.plot_model(model, "multi_input_and_output_model.png")
+
+
+tensors = model(test, training=False)
+
+hg = tensors[0].numpy()[0]
+ag = tensors[0].numpy()[1]
+
+print(f"{hg} : {ag}")
+#print(model.predict(test))
+model.save("cnp_3.h5")
