5a7373a0b4d6c91622141a1e2cac289ed9d49c4b
Philipp Staudinger <philipp.eckhard.staudinger@gmail.com>
2023-06-09 16:14:19 +0200

    .

--- main2.py
+++ main2.py
@@ -23,14 +23,14 @@ model.add(Dense(12, activation='linear'))
 model.add(Dense(10, activation='linear'))
 model.add(Dense(2, activation='relu'))
 
-test = [[10,1,-2.2,3,1,-5.100000000000001,-10,1,1.4999999999999991,-8,-2,-1.5999999999999996,3,-1,-3.3000000000000007,-4,13,1,1.9000000000000004,-1,-2,-4.300000000000001,-15,1,-1.6,-7,-2,-2.1000000000000005,-7,-2,-3.5,-3]]
+test = [[5,-1,5.899999999999999,11,3,6.500000000000001,6,-1,2.7,-2,1,-0.5,1,3,3.8000000000000003,2,9,0,-0.5,-2,0,-0.5999999999999996,7,1,3.200000000000001,8,4,3.0000000000000004,-1,1,-0.7000000000000002,-4]]
 test = preprocessing.MinMaxScaler().fit_transform(x)
 
 
 model.compile(optimizer='adam', loss='mse', metrics = ['accuracy'])
 
 model.fit(x_train, y_train, batch_size=64, epochs=15, validation_data=(x_val, y_val))
-keras.utils.plot_model(model, "multi_input_and_output_model.png")
+#keras.utils.plot_model(model, "multi_input_and_output_model.png")
 
 
 tensors = model(test, training=False)
@@ -38,6 +38,6 @@ tensors = model(test, training=False)
 hg = tensors[0].numpy()[0]
 ag = tensors[0].numpy()[1]
 
-print(f"{hg} : {ag}")
+print(f"{int(hg)} : {int(ag)}")
 #print(model.predict(test))
 model.save("cnp_3.h5")
